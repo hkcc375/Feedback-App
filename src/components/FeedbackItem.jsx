4aeda485092800cb5e6bd17ed0a3c28e0fb8caf3
Hemant Kumar <hkcc375@gmail.com>
2023-11-23 19:51:34 +0530

    removed unused variables

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,5 +1,4 @@
 import { FaTimes, FaEdit } from "react-icons/fa";
-import PropTypes from "prop-types";
 import React from "react";
 import Card from "./shared/Card";
 import { useContext } from "react";
